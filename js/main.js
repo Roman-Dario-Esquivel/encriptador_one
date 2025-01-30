695488cf26e46c9cd86a30b7e5baf7ef6a79cee1
Roman-Dario-Esquivel <esquivelromandario@gmail.com>
2025-01-30 13:35:00 -0300

    funcional

--- js/main.js
+++ js/main.js
@@ -1,1 +1,95 @@
-const clean = document.getElementById('clean');
+const resultText = document.getElementById("resultText");
+const inputText = document.getElementById("inputText");
+const repairButton = document.getElementById("repair-button");
+const customAlert = document.getElementById("customAlert");
+const modalMessage = document.getElementById("modalMessage");
+
+// Dictionary
+
+const llaves = {
+    'e': 'enter',
+    'i': 'imes',
+    'a': 'ai',
+    'o': 'ober',
+    'u': 'ufat'
+}
+
+// Repair text
+
+const removeAccents = (str) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+function repair(){
+    let textRepair = removeAccents(inputText.value);
+    textRepair = textRepair.toLowerCase();
+    inputText.value = textRepair;
+    inputText.focus();
+    repairButton.style.display = "none";
+}
+// Validate text
+
+function validateTexto(){
+    let textwrite = inputText.value;
+    let validador = textwrite.match(/^[a-z\s]*$/);
+
+    if(!validador || validador == 0) {
+        showModal('Solo son permitidas letras minúsculas y sin acentos');
+        
+        repairButton.style.display = "block";
+        return false;
+    }
+    return true;
+}
+
+// Encrypt text
+function encrypt(){
+    if(validateTexto()) {
+        let texto = inputText.value;
+        for (let key in llaves) {
+            texto = texto.replaceAll(key, llaves[key]);
+        }
+        resultText.value = texto;
+        repairButton.style.display = "none";
+    }
+}
+
+// Desencrypt text
+
+function decrypt(){
+    if(validateTexto()) {
+        let texto = inputText.value;
+        for (let key in llaves) {
+            texto = texto.replaceAll(llaves[key],key) ;
+        }
+        resultText.value = texto;
+        repairButton.style.display = "none";
+    }
+}
+
+// Copy text
+
+function copy() {
+    
+    resultText.select();
+    navigator.clipboard.writeText(resultText.value)
+    alert("Texto Copiado")
+}
+
+// Clear text
+function clean(){
+    inputText.value = "";
+    resultText.value = "";
+    inputText.focus();
+}
+
+// Mostrar modal personalizado
+function showModal(message) {
+    modalMessage.textContent = message;
+    customAlert.style.display = "block";
+}
+
+// Cerrar modal personalizado
+function closeModal() {
+    customAlert.style.display = "none";
+}
